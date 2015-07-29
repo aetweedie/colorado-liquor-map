6b3437b59013c2bd9e3cbda2e56e849dad067410
Alan Tweedie <aetweedie@gmail.com>
2015-07-29 10:51:33 -0600

    Working map with points plotted and icons displaying

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -10,6 +10,30 @@ Orbit.prototype.get = function (path, fn) {
 
 function initialize() {
 
+  var licenseLocations = new Orbit();
+
+  var mapOptions = {
+    center: { lat: 39.102100, lng: -105.536616},
+    zoom: 7
+  };
+
+  licenseLocations.get ('/licenses', function() {
+    var licenses = JSON.parse (this.response);
+    for (var i = 0; i < licenses.length; i ++) {
+      var latlng = new google.maps.LatLng(licenses[i].Latitude, licenses[i].Longitude);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: latlng,
+          icon: '/images/liquor.png'
+      });
+      }
+  });
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+        // map.mapTypes.set('map_style', styledMap);
+        // map.setMapTypeId('map_style');
+
   var styleArray = [
   {
     featureType: "road.highway",
@@ -44,61 +68,13 @@ function initialize() {
   }
 ];
 
+map.setOptions({styles: styleArray});
+
 var styledMap = new google.maps.StyledMapType(styleArray,
     {name: "Styled Map"});
 
-  var mapOptions = {
-    center: { lat: 39.102100, lng: -105.536616},
-    zoom: 7,
-    style: styleArray,
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-        map.mapTypes.set('map_style', styledMap);
-        map.setMapTypeId('map_style');
-        var licenseLocations = new Orbit();
-
-        licenseLocations.get ('/licenses', function() {
-          var licenses = JSON.parse (this.response);
-          var geocoder = new google.maps.Geocoder();
-          for (var i = 0; i < licenses.length; i ++) {
-              geocoder.geocode( { 'address': licenses[i].street + licenses[i].city + licenses[i].state + licenses[i].zip}, function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                  map.setCenter(results[0].geometry.location);
-                  console.log(licenses);
-                  var marker = new google.maps.Marker({
-                      map: map,
-                      position: results[0].geometry.location,
-                      icon: '/images/liquor.png'
-                  });
-                } else {
-                  alert("Geocode was not successful for the following reason: " + status);
-                }
-                //     var contentString =
-                //   '<div id="content">'+
-                //   '<div id="siteNotice">'+
-                //   '</div>'+
-                //   '<h1 id="firstHeading" class="firstHeading">' + businessName + '</h1>' +
-                //   '<h2 id="secondHeading" class="secondHeading">' + street + '</h2>' +
-                //   '</div>'+
-                //   '</div>';
-                //
-                //   var infowindow = new google.maps.InfoWindow({
-                //   content: contentString
-                // });
-                // var marker = new google.maps.Marker({
-                //   position: myLatlng,
-                //   map: map,
-                //   title: licenses.businessName
-                // });
-                // google.maps.event.addListener(marker, 'click', function() {
-                //   infowindow.open(map,marker);
-                // });
-              });
-            }
-        });
-
 }
+
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
